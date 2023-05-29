3f4591bea4b27365308ca2146a700762b7c35b2b
AlexeyKud <renenkton123@yandex.ru>
2023-05-29 22:09:38 +0300

    Проект 4. Ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,11 @@
 const openPopupButtonEl = document.querySelector('#open-popup-button');
 const closePopupButtonEl = document.querySelector('#close-popup-button');
 const editPopupEl = document.querySelector('#edit-popup');
-const pageTitleEl = document.querySelector('.page__title');
+const pageTitleEl = document.querySelector('.profile__title');
 const nameInputEl = document.querySelector('#name-input');
 const editFormEl = document.querySelector('#edit-form');
+const inputSubTitleEl= document.querySelector('#input-subtitle');
+const profileSubTitleEl = document.querySelector('.profile__subtitle'); 
 
 openPopupButtonEl.addEventListener('click', function () {
   openPopup(editPopupEl);
@@ -14,11 +16,13 @@ closePopupButtonEl.addEventListener('click', function () {
 });
 
 nameInputEl.value = pageTitleEl.textContent;
+inputSubTitleEl.value = profileSubTitleEl.textContent;
 
 editFormEl.addEventListener('submit', function (event) {
   event.preventDefault();
 
   pageTitleEl.textContent = nameInputEl.value;
+  profileSubTitleEl.textContent = inputSubTitleEl.value;
 
   closePopup(editPopupEl);
 });
