a84fd7c0806b4ef0ed772b9ecac960fe613b2774
AlexeyKud <renenkton123@yandex.ru>
2023-07-18 21:10:29 +0300

    Проект 5 на ревью

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,8 @@ const editFormEl = document.querySelector('#edit-form');
 const inputSubTitleEl= document.querySelector('#input-subtitle');
 const profileSubTitleEl = document.querySelector('.profile__subtitle'); 
 
+
+// открытие попапа по нажатию 
 openPopupButtonEl.addEventListener('click', function () {
   openPopup(editPopupEl);
   nameInputEl.value = pageTitleEl.textContent;
@@ -16,6 +18,9 @@ openPopupButtonEl.addEventListener('click', function () {
 closePopupButtonEl.addEventListener('click', function () {
   closePopup(editPopupEl);
 });
+
+
+// отправка названий на сайт
 
 editFormEl.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -33,3 +38,22 @@ function openPopup(popupEl) {
 function closePopup(popupEl) {
   popupEl.classList.remove('popup_is-opened');
 }
+
+
+// попапы добавления карточек
+
+const openAddPopupBtn= document.querySelector('#add-popup-button');
+const closeAddPopupBtn= document.querySelector('#close-popup-add');
+const addPopupEl = document.querySelector('#add-popup');
+const addForm= document.querySelector('#add-form');
+
+
+openAddPopupBtn.addEventListener('click', function () {
+  nameInputEl.value = pageTitleEl.textContent;
+  inputSubTitleEl.value = profileSubTitleEl.textContent;
+openPopup(addPopupEl);
+});
+
+closeAddPopupBtn.addEventListener('click', function () {
+closePopup(addPopupEl);
+});
